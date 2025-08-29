63d743cd3c012e8cca5661551604f572f0b9e939
gitdotyemi <mailedtoyemi@gmail.com>
2025-08-29 23:08:56 +0100

    29/08/2025

--- oop/person.js
+++ oop/person.js
@@ -121,3 +121,65 @@ const you = new Person("Andrew", "Mead", 27)
 me.setName("Tosin Fayemi")
 console.log(me.getBio())
 console.log(you)
+
+const Person = function (firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+}
+
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age} years old.`
+
+    this.likes.forEach((like) => {
+        bio = bio + ` ${this.firstName} likes ${like}.`
+    })
+
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(" ")
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+
+const me = new Person("Opeyemi", "Bello", 25, ["Video games", "Music"])
+const you = new Person("Andrew", "Mead", 27)
+
+me.setName("Tosin Fayemi")
+console.log(me.getBio())
+console.log(you)
+
+const Person = function (firstName, lastName, age, likes = []) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.likes = likes
+}
+
+Person.prototype.getBio = function () {
+    let bio = `${this.firstName} is ${this.age} years old.`
+
+    this.likes.forEach((like) => {
+        bio = bio + ` ${this.firstName} likes ${like}.`
+    })
+
+    return bio
+}
+
+Person.prototype.setName = function (fullName) {
+    const names = fullName.split(" ")
+    this.firstName = names[0]
+    this.lastName = names[1]
+}
+
+
+const me = new Person("Opeyemi", "Bello", 25, ["Video games", "Music"])
+const you = new Person("Andrew", "Mead", 27)
+
+me.setName("Tosin Fayemi")
+console.log(me.getBio())
+console.log(you)
